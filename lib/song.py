b997747617c629b40684eb7aa312be9ee17a6f5d
kiplangatwayne <waynekiplangatkorir903@gmail.com>
2023-08-30 14:13:57 +0300

    completed  Assingnment

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,48 @@
-from config import CONN, CURSOR
+# lib/song.py
+
+import sqlite3
 
 class Song:
-    pass
+
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+        # Establish a connection and create a cursor
+        conn = sqlite3.connect("music.db")
+        cursor = conn.cursor()
+
+        cursor.execute(sql)
+        conn.commit()
+        conn.close()
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+        # Establish a connection and create a cursor
+        conn = sqlite3.connect("music.db")
+        cursor = conn.cursor()
+
+        cursor.execute(sql, (self.name, self.album))
+        conn.commit()
+
+        self.id = cursor.lastrowid
+        conn.close()
+
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
